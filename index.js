c8221019be7f359a66e00f33557b67ebadcd5518
Jordan Stebbins <jstebbin14@gmail.com>
2021-06-27 19:19:10 -0400

    Set up project

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import cors from "cors";
+import login from "./api/login.route.js";
+
+
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const MongoClient = mongodb.MongoClient;
 
-express()
+dotenv.config();
+
+MongoClient.connect(
+  process.env.DB_URI,
+  {
+    poolSize: 50,
+    wtimeout: 2500,
+    useNewUrlParser: true
+  }
+).catch(err => {
+  console.error(err.stack)
+  process.exit(1)
+}).then(async client => {
+  express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(cors())
+  .use(express.json())
+  .use("/api/v1/login", login)
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+})
+
+
 
   
