5be49ca920d97c723ecf005f295cf1eef106d12c
Dpk417182 <derrickkeough@yahoo.com>
2021-07-08 12:31:46 -0400

    flesh out a basic login endpoint

--- views/pages/App.js
+++ views/pages/App.js
@@ -22,9 +22,29 @@ MongoClient.connect(url, function(err, client) {
 });
 
 function loginAPI(){
-}
+    const doLogin = async event =>
+    {
+        event.preventDefault();
+
+        alert('doIt()');
+    };
+
+    return (
+        <div id="loginDiv">
+            <form onSubmit={doLogin}>
+            <span id="inner-title">PLEASE LOG IN</span><br />
+            <input type="text" id="loginName" placeholder="Username" /><br />
+            <input type="password" id="loginPassword" placeholder="Password" /><br />
+            <input type="submit" id="loginButton" class="buttons" value = "Do It" onClick={doLogin} />
+            </form>
+            <span id="loginResult"></span>
+        </div>
+    );
+};
+
 function blockSwitch(){
 }
+
 function registerAPI(){
 }
 
