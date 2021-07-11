a87b510a5fed1e1bd991fbcfd3c539ad7f16c22b
Dpk417182 <derrickkeough@yahoo.com>
2021-07-11 12:43:29 -0400

    include .env, split api from server.js

---
+++ api.js
@@ -0,0 +1,159 @@
+
+// app.post('/api/login', async (req, res, next) => 
+// {
+//   // incoming: login, password
+//   // outgoing: id, firstName, lastName, error
+
+//  var error = '';
+
+//   const { login, password } = req.body;
+
+//   const db = client.db();
+//   const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+
+//   var id = -1;
+//   var fn = '';
+//   var ln = '';
+
+//   if( results.length > 0 )
+//   {
+//     id = results[0].UserId;
+//     fn = results[0].FirstName;
+//     ln = results[0].LastName;
+//   }
+
+//   var ret = { id:id, firstName:fn, lastName:ln, error:''};
+//   res.status(200).json(ret);
+// });
+
+// app.post('/api/addcard', async (req, res, next) =>
+// {
+//   // incoming: userId, color
+//   // outgoing: error
+	
+//   const { userId, card } = req.body;
+
+//   const newCard = {Card:card,UserId:userId};
+//   var error = '';
+
+//   try
+//   {
+//     const db = client.db();
+//     const result = db.collection('Cards').insertOne(newCard);
+//   }
+//   catch(e)
+//   {
+//     error = e.toString();
+//   }
+
+//   cardList.push( card );
+
+//   var ret = { error: error };
+//   res.status(200).json(ret);
+// });
+
+// app.post('/api/searchcards', async (req, res, next) => 
+// {
+//   // incoming: userId, search
+//   // outgoing: results[], error
+
+//   var error = '';
+
+//   const { userId, search } = req.body;
+
+//   var _search = search.trim();
+//   
+//   const db = client.db();
+//   const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
+//   
+//   var _ret = [];
+//   for( var i=0; i<results.length; i++ )
+//   {
+//     _ret.push( results[i].Card );
+//   }
+//   
+//   var ret = {results:_ret, error:error};
+//   res.status(200).json(ret);
+// });
+
+exports.setApp = function ( app, client )
+{
+
+    app.post('/api/addcard', async (req, res, next) =>
+    {
+      // incoming: userId, color
+      // outgoing: error
+        
+      const { userId, card } = req.body;
+    
+      const newCard = {Card:card,UserId:userId};
+      var error = '';
+    
+      try
+      {
+        const db = client.db();
+        const result = db.collection('Cards').insertOne(newCard);
+      }
+      catch(e)
+      {
+        error = e.toString();
+      }
+    
+      cardList.push( card );
+    
+      var ret = { error: error };
+      res.status(200).json(ret);
+    });
+    
+    app.post('/api/login', async (req, res, next) => 
+    {
+      // incoming: login, password
+      // outgoing: id, firstName, lastName, error
+    
+     var error = '';
+    
+      const { login, password } = req.body;
+    
+      const db = client.db();
+      const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+    
+      var id = -1;
+      var fn = '';
+      var ln = '';
+    
+      if( results.length > 0 )
+      {
+        id = results[0].UserId;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
+      }
+    
+      var ret = { id:id, firstName:fn, lastName:ln, error:''};
+      res.status(200).json(ret);
+    });
+    
+    app.post('/api/searchcards', async (req, res, next) => 
+    {
+      // incoming: userId, search
+      // outgoing: results[], error
+    
+      var error = '';
+    
+      const { userId, search } = req.body;
+    
+      var _search = search.trim();
+      
+      const db = client.db();
+      const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
+      
+      var _ret = [];
+      for( var i=0; i<results.length; i++ )
+      {
+        _ret.push( results[i].Card );
+      }
+      
+      var ret = {results:_ret, error:error};
+      res.status(200).json(ret);
+    });
+    
+}
