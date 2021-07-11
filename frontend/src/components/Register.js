6563e29832fd2294ac5aafc7e5e0957761cfcd47
jmurphyinbox@gmail.com <jmurphy@gmail.com>
2021-07-11 00:53:59 -0400

    Add Register.js to components

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+// Will update with methods and API later -JMM
+function Register(){
+    return (
+        <div className="RegisterBlock">
+            <Text>First Name</Text>
+            <input type="text" id="first"></input>
+            <Text>Last Name</Text>
+            <input type="text" id="last"></input>
+            <Text>Phone Number</Text>
+            <input type="text" id="phone"></input>
+            <Text>Email</Text>
+            <input type="text" id="email"></input>
+            <Text>New Password</Text>
+            <input type="password" id="pw"></input>
+            <Text>Verify Password</Text>
+            <input type="password" id="verifypw"></input>
+            <Button onClick={ }>Register</Button>
+            <span>{ }</span> // Error block ((Will update when API links))
+        </div>
+    );
+};
