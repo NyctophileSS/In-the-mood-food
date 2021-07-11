82eb417d98c92982c2456a7cc2151f31f18fa470
jmurphyinbox@gmail.com <jmurphy@gmail.com>
2021-07-11 00:42:32 -0400

    Add linebreaks in JSX of Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -58,11 +58,11 @@ function Login()
       <div id="loginDiv">
         <form onSubmit={doLogin}>
         <span id="inner-title">PLEASE LOG IN</span><br />
-        <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} />
-        <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} />
+        <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} /><br/>
+        <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} /><br/>
         <input type="submit" id="loginButton" class="buttons" value = "Do It"
           onClick={doLogin} />
-        </form>
+        </form><br/>
         <span id="loginResult">{message}</span>
      </div>
     );
