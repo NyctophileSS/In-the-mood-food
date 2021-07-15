f334ef858e596d7204fea54e1da509c0ea7b9241
jmurphyGoKnights22 <85531257+jmurphyGoKnights22@users.noreply.github.com>
2021-07-14 23:15:18 -0400

    Update Register.js password comparison

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -24,7 +24,7 @@ function Register()
                     phoneNumber:loginPhoneNumber.value};
         var js = JSON.stringify(obj);
 
-        if((loginPassword1 == loginPassword2) == false){
+        if((loginPassword1.value != loginPassword2.value){
             setMessage("These passwords don't match. Please Try again.");
             return;
         }
