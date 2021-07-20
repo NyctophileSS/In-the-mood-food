605235d22df6381fd0bcd569503f9bf551580b8f
jmurphyinbox@gmail.com <jmurphy@gmail.com>
2021-07-19 22:53:16 -0400

    Add const for values QuizQuestions.js

--- frontend/src/components/QuizQuestions.js
+++ frontend/src/components/QuizQuestions.js
@@ -2,16 +2,43 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 
+
 function QuizQuestions(){
+    
+    var priceRange;
+    var distanceRadius;
+    var cuisineInput;
+    var rating;
+
+    const [price,setPrice] = useState(priceRange.value);
+    const [distance,setDistance] = useState(distanceRadius.value);
+
+    const doQuizQuery = async event =>{
+
+    }
+    
     return(
         <div>
             <form>
                 <label>What price range are you interested in?</label>
-                <input type="range" min='0' max='4'></input>
-                <label>What distance are you willing to drive</label>
-                <input type="range" min='1' max='25'></input>
-                <label>Please select what food you are "In-The-Mood" for:</label>
-                <input type="checkbox"></input>
+                <input id='price' type="range" min='0' max='4' ref={(c) => priceRange = c}></input>
+                <p id='priceValue'>{price}</p>
+                <label for='distance'>What distance are you willing to drive</label>
+                <input id='distance' type="range" min='1' max='25' ref={(c) => distanceRadius = c}></input>
+                <p id='distanceValue'>{distance} miles</p>
+                <label for='cuisine'>Please provide what food you are "In-The-Mood" for:</label>
+                <input id='cuisine' type="text" placeholder='Ex. "American", "Burgers", "Pasta", etc.' ref={(c) => cuisineInput = c}></input>
+                <label for='rating'>Provide a preffered rating:</label>
+                <select id='rating' ref={(c) => rating = c}>
+                    <option value='2'>2.0 and up</option>
+                    <option value='2.5'>2.5 and up</option>
+                    <option value='3'>3.0 and up</option>
+                    <option value='3.5'>3.5 and up</option>
+                    <option value='4'>4 and up</option>
+                    <option value='4.5'>4.5 and up</option>
+                    <option value='5'>5.0 and up</option>
+                </select>
+                <a id='QuizSearch' onClick={doQuizQuery}>Search</a>
             </form>
         </div>
     )
