1963d6c541d6bf4c62af1a5a993c49755fe200b0
jmurphyinbox@gmail.com <jmurphy@gmail.com>
2021-07-19 23:34:18 -0400

    Add GoogleMap.js component to ResultsPage.js

--- frontend/src/pages/ResultsPage.js
+++ frontend/src/pages/ResultsPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import GoogleMap from '../components/GoogleMap';
+import PageTitle from '../components/PageTitle';
 
 const ResultsPage = () =>{
     return(
         <div>
-
+            <PageTitle/>
+            <GoogleMap/>
         </div>
     );
 };
