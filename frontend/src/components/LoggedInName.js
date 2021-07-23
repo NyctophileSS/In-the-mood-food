ac662d981d70154aa6ec60550aac127518c209a2
Dpk417182 <derrickkeough@yahoo.com>
2021-07-22 21:26:56 -0400

    only whitespace seems changed (commiting in case)

--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -18,14 +18,13 @@ function LoggedInName()
 
     };    
 
-  return(
-   <div id="loggedInDiv">
-   <span id="userName">Logged In As {firstName} {lastName}</span><br />
-   <button type="button" id="logoutButton" class="buttons" 
-     onClick={doLogout}> Log Out </button>
-   </div>
-  );
-
+    return(
+     <div id="loggedInDiv">
+    <span id="userName">Logged In As {firstName} {lastName}</span><br />
+    <button type="button" id="logoutButton" class="buttons" 
+      onClick={doLogout}> Log Out </button>
+     </div>
+    );
 };
 
 export default LoggedInName;
