7fd7efe9df77993b9d3fbbd75cc0b594599c32c3
GrahamMWest <grahamwest@knights.ucf.edu>
2021-07-24 14:21:52 -0400

    added import for maps api

--- frontend/src/components/GoogleMap.js
+++ frontend/src/components/GoogleMap.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles.css';
 import './Maps.js';
-//import "https://maps.googleapis.com/maps/api/js?key=AIzaSyDZkoQ8wRK8iu9EAu7upcK2zynH6fM3p-I&callback=initMap&libraries=places&v=weekly"; 
+import "https://maps.googleapis.com/maps/api/js?key=AIzaSyDZkoQ8wRK8iu9EAu7upcK2zynH6fM3p-I&callback=initMap&libraries=places&v=weekly"; 
 //async;
 
 function GoogleMap (){
