6ad32c454c7cbd18bfbe0e7f19ea4e127001f17f
GrahamMWest <grahamwest@knights.ucf.edu>
2021-07-24 13:55:46 -0400

    adding import header

--- frontend/src/components/GoogleMap.js
+++ frontend/src/components/GoogleMap.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles.css';
-//import './Maps.js';
+import './Maps.js';
 //import "https://maps.googleapis.com/maps/api/js?key=AIzaSyDZkoQ8wRK8iu9EAu7upcK2zynH6fM3p-I&callback=initMap&libraries=places&v=weekly"; 
 //async;
 
