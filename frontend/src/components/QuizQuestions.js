6d7d0208ba1b6e0042a1c6729ac1f8f5e5050f2d
elierbermudez <bermudezelier@gmail.com>
2021-07-25 19:22:32 -0400

    Format QuizQuestions page

--- frontend/src/components/QuizQuestions.js
+++ frontend/src/components/QuizQuestions.js
@@ -1,41 +1,52 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-export default function QuizQuestions(){
-    
+export default function QuizQuestions() {
+
     var distanceRadiusMeters;
     var distanceRadiusMiles;
     var cuisineInput;
     var rating;
     var price;
-    
-    const doQuizQuery = async event =>{    
-        distanceRadiusMeters = distanceRadiusMiles / 0.00062137; 
+
+    const doQuizQuery = async event => {
+        distanceRadiusMeters = distanceRadiusMiles / 0.00062137;
     }
 
-    return(
-        <div>
+    return (
+        <div id="questionsDiv">
             <form>
-                <label for='price'>What price range are you interested in?</label>
-                <p><select id='price' ref={(c) => price = c}>
-                    <option value='1'>$</option>
-                    <option value='2'>$$</option>
-                    <option value='3'>$$$</option>
-                    <option value='4'>$$$$</option>
-                </select></p>
-                <label for='distance'>What distance are you willing to drive</label>
-                <p><input id='distance' type='number' min={0} max={50} step={5} ref={(c) => distanceRadiusMiles = c}></input> miles</p>
-                <label for='cuisine'>Please provide what food you are "In-The-Mood" for:</label>
-                <p><input id='cuisine' type="text" placeholder='Ex. "American", "Burgers", "Pasta", etc.' ref={(c) => cuisineInput = c}></input></p>
-                <label for='rating'>Provide a preffered rating:</label>
-                <p><select id='rating' ref={(c) => rating = c}>
-                    <option value='2'>2.0 and up</option>
-                    <option value='2.5'>2.5 and up</option>
-                    <option value='3'>3.0 and up</option>
-                    <option value='3.5'>3.5 and up</option>
-                    <option value='4'>4 and up</option>
-                    <option value='4.5'>4.5 and up</option>
-                    <option value='5'>5.0 and up</option>
-                </select></p>
+                <div id="q1">
+                    <div><label for='price'>What price range are you interested in?</label></div>
+                    <div><select id='price' ref={(c) => price = c}>
+                        <option value='1'>$</option>
+                        <option value='2'>$$</option>
+                        <option value='3'>$$$</option>
+                        <option value='4'>$$$$</option>
+                    </select></div>
+                </div>
+
+                <div id="q2">
+                    <div><label for='distance'>What distance are you willing to drive</label></div>
+                    <div><input id='distance' type='number' min={0} max={50} step={5} ref={(c) => distanceRadiusMiles = c}></input> miles</div>
+                </div>
+
+                <div id="q3">
+                    <div><label for='cuisine'>Please provide what food you are "In-The-Mood" for:</label></div>
+                    <div><input id='cuisine' type="text" placeholder='Ex. "American", "Burgers", "Pasta", etc.' ref={(c) => cuisineInput = c}></input></div>
+                </div>
+
+                <div id="q4">
+                <div><label for='rating'>Provide a preferred rating:</label></div>
+                    <div><select id='rating' ref={(c) => rating = c}>
+                        <option value='2'>2.0 and up</option>
+                        <option value='2.5'>2.5 and up</option>
+                        <option value='3'>3.0 and up</option>
+                        <option value='3.5'>3.5 and up</option>
+                        <option value='4'>4 and up</option>
+                        <option value='4.5'>4.5 and up</option>
+                        <option value='5'>5.0 and up</option>
+                    </select></div>
+                </div>
                 <button id='QuizSearch' onClick={doQuizQuery}>Search</button>
             </form>
         </div>
