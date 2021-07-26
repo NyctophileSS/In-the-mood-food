5524e4bd08314d1d4ae8603491ae4d6e17ec6cd2
elierbermudez <bermudezelier@gmail.com>
2021-07-26 19:35:53 -0400

    Center logout button

--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -21,8 +21,8 @@ function LoggedInName()
 
     return(
      <div id="loggedInDiv">
-    <span id="userName">Logged In As {firstName} {lastName}</span><br/>
-    <button type="button" id="logoutButton" class="buttons" 
+      <div><span id="userName">Logged In As {firstName} {lastName}</span></div>
+      <button type="button" id="logoutButton" class="buttons" 
       onClick={doLogout}> Log Out </button>
      </div>
     );
