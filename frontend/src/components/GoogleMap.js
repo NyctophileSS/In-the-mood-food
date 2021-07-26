6f2c59db9554c7df540fc69310f5e4981f297f20
jmurphyinbox@gmail.com <jmurphy@gmail.com>
2021-07-25 23:04:12 -0400

    Fix GoogleMap.js

--- frontend/src/components/GoogleMap.js
+++ frontend/src/components/GoogleMap.js
@@ -50,7 +50,3 @@ class MapDiv extends React.Component() {
         );
     };
 };
-
-export default GoogleApiWrapper({
-    apiKey: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDZkoQ8wRK8iu9EAu7upcK2zynH6fM3p-I&callback=initMap&libraries=places&v=weekly'
-})(MapDiv);
