b7c6e7b5b1c0e22934fa64fa221a458cb6ae3550
Olamijide <51564294+Olamijide@users.noreply.github.com>
2021-07-27 00:07:23 -0500

    Buttons and effects
    As per James' request.  If it doesn't work, I'd have to revert to the old git.

--- frontend/src/components/QuizQuestions.js
+++ frontend/src/components/QuizQuestions.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { motion } from "framer-motion";
 
 export default function QuizQuestions() {
 
@@ -16,28 +17,45 @@ export default function QuizQuestions() {
     return (
         <div id="questionsDiv">
             <form>
-                <div id="q1">
-                    <div><label for='price'>What's your budget?</label></div>
-                    <div><select id='price' ref={(c) => price = c}>
+                <motion.div id="q1"
+                    initial = {{opacity: 0}}
+                    animate = {{opacity: 1}}
+                    transition = {{delay: 0.5, duration:1.5}}
+                >
+                <div><label for='price'>What's your budget?</label></div>
+
+                <div> <select id='price' ref={(c) => price = c}>
                         <option value='1'>$</option>
                         <option value='2'>$$</option>
                         <option value='3'>$$$</option>
                         <option value='4'>$$$$</option>
                     </select></div>
-                </div>
+                </motion.div>
 
-                <div id="q2">
-                    <div><label for='distance'>What's your max distance?</label></div>
-                    <div><input id='distance' type='number' min={0} max={50} step={5} 
+                <motion.div id="q2"
+                      initial = {{opacity: 0}}
+                      animate = {{opacity: 1}}
+                      transition = {{delay: 1, duration:1.5}}
+                >
+                    <label for='distance'>What's your max distance?</label><div>
+                    <input id='distance' type='number' min={0} max={50} step={5} 
                     ref={(c) => distanceRadiusMiles = c}></input> miles</div>
-                </div>
+                </motion.div>
 
-                <div id="q3">
+                <motion.div id="q3"
+                      initial = {{opacity: 0}}
+                      animate = {{opacity: 1}}
+                      transition = {{delay: 1.5, duration:1.5}}
+                >
                     <div><label for='cuisine'>What food d'ya want?</label></div>
                     <div><input id='cuisine' type="text" placeholder='"Burgers", etc.' ref={(c) => cuisineInput = c}></input></div>
-                </div>
+                </motion.div>
 
-                <div id="q4">
+                <motion.div id="q4"
+                      initial = {{opacity: 0}}
+                      animate = {{opacity: 1}}
+                      transition = {{delay: 2, duration:1.5}}
+                >
                     <div><label for='rating'>What's your preferred rating?</label></div>
                     <div><select id='rating' ref={(c) => rating = c}>
                         <option value='2'>2.0 and up</option>
@@ -48,7 +66,7 @@ export default function QuizQuestions() {
                         <option value='4.5'>4.5 and up</option>
                         <option value='5'>5.0 and up</option>
                     </select></div>
-                </div>
+                </motion.div>
                 <button type="button" id='QuizSearch' onClick={doQuizQuery}>Search</button>
             </form>
         </div>
