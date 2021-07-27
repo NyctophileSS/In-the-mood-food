90a5403c0f018868702bc148934a26ed1d6f917c
GrahamMWest <grahamwest@knights.ucf.edu>
2021-07-27 06:24:57 -0400

    trying js

--- frontend/src/components/GoogleMap.js
+++ frontend/src/components/GoogleMap.js
@@ -44,7 +44,7 @@ export default class MapDiv extends Component {
 
         var foodLocation = new window.google.maps.LatLng(lat, lng);
 
-        radiusCircle = new window.google.maps.Circle({
+        radiusCircle = new window.google.maps.Circle(document.getElementById('Map'), {
             strokeColor: "#6a62d2",
             strokeOpacity: 0.8,
             strokeWeight: 2,
