e10f2389e812f6f4b4b9d25068b74a3eae9f02a1
jmurphyinbox@gmail.com <jmurphy@gmail.com>
2021-07-27 08:23:45 -0400

    Fix button

--- frontend/src/components/buttonToQuiz.js
+++ frontend/src/components/buttonToQuiz.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import '../styles.css';
+import { motion } from "framer-motion";
+
+const buttonVariants = {
+    hover: {
+        scale: 1.1,
+        transition: {
+            duration: 0.3,
+            yoyo: 5
+        }
+    }
+}
 
 export default function buttonToQuiz (){
     const goToQuiz = async event => {
         window.location.href = '/quiz'
     }
     return(
-        <input id="mapToQuizButton" type="button" onClick={goToQuiz}>Let's Search For Something Else!</input>
-    );
+        <p><motion.button
+            variants={buttonVariants}
+            whileHover="hover"
+
+            type="button" id="mapToQuizButton" onClick={gotToQuiz}> Let's Search Again!</motion.button></p>
+    )
 };
